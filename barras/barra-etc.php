d3ee594ad47af9d64a7aaf5d11b992a84218d005
jenyferlima <38404317+jenyferlima@users.noreply.github.com>
2018-06-22 22:10:35 -0300

    Add files via upload

---
+++ barras/barra-etc.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <title>Barra ETC Brasil</title>
+    <link rel="stylesheet" href="https://etcbrasil.com/wp-content/themes/ETCBr/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://etcbrasil.com/wp-content/themes/ETCBr/css/hover-min.css">
+    <link rel="stylesheet" href="https://etcbrasil.com/wp-content/themes/ETCBr/css/animate.min.css">
+    <link rel="stylesheet" href="https://etcbrasil.com/wp-content/themes/ETCBr/style.css">
+    <script src="https://etcbrasil.com/wp-content/themes/ETCBr/js/bootstrap.min.js"></script>
+    
+    <meta name="description" content="Educational Technology Consulting" />
+    <meta property="og:title" content="ETC Brasil" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="Educational Technology Consulting" />
+    <meta property="og:site_name" content="ETC Brasil" />
+    <meta property="og:url" content="https://etcbrasil.com/" />
+    <meta property="og:image" type="image/png" content="https://etcbrasil.com/wp-content/themes/ETCBr/img/etc_logo.png" />
+
+    <link rel="shortcut icon" type="image/png" href="https://etcbrasil.com/wp-content/uploads/2018/05/favicon-10.png"/>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    
+    <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js'></script>
+
+  </head>
+  <body>
+		<div class="barra">
+      <div class="container">
+        <div class="row justify-content-center">
+          <nav class="navbar navbar-expand-lg" style="padding:2px">
+          <a class="navbar-brand" href="https://etcbrasil.com/">
+        <span style="padding:2px;"><img src="https://etcbrasil.com/wp-content/themes/ETCBr/img/icone_etc.png" alt="Ícone ETC Brasil"></span>ETC BRASIL
+        </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"><i class="fas fa-sort-down" style="color:#395D68"></i></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="http://etcbrasil.com/nossos-parceiros/">NOSSOS PARCEIROS</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">NOSSOS SITES</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">ÁREA DO CLIENTE</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.facebook.com/etcbrasil/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="https://www.instagram.com/etcbrasil/" target="_blank"><i class="fab fa-instagram"></i></a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="https://twitter.com/etcbrasil_" target="_blank"><i class="fab fa-twitter"></i></a>
+              </li>
+
+          <li class="nav-item">
+                <a class="nav-link" href="https://www.linkedin.com/company/jewc-2012/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+              </li>
+
+          <li class="nav-item">
+                <a class="nav-link" href="https://www.youtube.com/channel/UCHl48IgXwZKJMI7zKYRynaQ" target="_blank"><i class="fab fa-youtube"></i></a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        </div>
+      </div>
+    </div>
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="https://etcbrasil.com/wp-content/themes/ETCBr/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ barras/footer-etc.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="pt-BR">
+  <head>
+		<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <title>Footer ETC Brasil</title>
+    <link rel="stylesheet" href="https://etcbrasil.com/wp-content/themes/ETCBr/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://etcbrasil.com/wp-content/themes/ETCBr/css/hover-min.css">
+    <link rel="stylesheet" href="https://etcbrasil.com/wp-content/themes/ETCBr/css/animate.min.css">
+    <link rel="stylesheet" href="https://etcbrasil.com/wp-content/themes/ETCBr/style.css">
+    <script src="https://etcbrasil.com/wp-content/themes/ETCBr/js/bootstrap.min.js"></script>
+    
+    <meta name="description" content="Educational Technology Consulting" />
+    <meta property="og:title" content="ETC Brasil" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="Educational Technology Consulting" />
+    <meta property="og:site_name" content="ETC Brasil" />
+    <meta property="og:url" content="https://etcbrasil.com/" />
+    <meta property="og:image" type="image/png" content="https://etcbrasil.com/wp-content/themes/ETCBr/img/etc_logo.png" />
+
+    <link rel="shortcut icon" type="image/png" href="https://etcbrasil.com/wp-content/uploads/2018/05/favicon-10.png"/>
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+    
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    
+    <script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js'></script>
+
+  </head>
+  <body>
+		<footer style="padding:15px">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-4 col-lg-1">
+          <a href="https://etcbrasil.com/" class="hvr-bob" target="_blank"><img src="https://etcbrasil.com/wp-content/themes/ETCBr/img/logo_etc.png" alt="" class="img-fluid mx-auto d-block"></a>
+        </div>
+        
+        <div class="col-8 col-lg-11">
+          <h4>ETC Brasil © 2018 - Todos os direitos reservados</h4>
+        </div>
+      </div>
+    </div>
+  </footer>
+    
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="https://etcbrasil.com/wp-content/themes/ETCBr/js/bootstrap.min.js"></script>
+  </body>
+</html>
